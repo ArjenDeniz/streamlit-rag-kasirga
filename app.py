de6789c17f5335422757989ec63a39c98121b037
unknown <firdevssuaydin17@gmail.com>
2025-09-03 17:56:22 +0200

    Cahanged template.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 open_api_key= os.getenv("key")
 DOCUMENTS_FOLDER= "docs"
 
-st.title(" Quick Rag App")
+st.title(" Muhteşem Karnak")
 tab1,  tab2 = st.tabs(["Chat","Create Vector Database"])
 @st.cache_data
 def load_documents_from_folder(folder_path):
@@ -73,8 +73,16 @@ def setup_rag(api_key):
 
     prompt_template = PromptTemplate(
         input_variables=["input", "documents"],
-        template = """Use the following documents to answer the question.
-        If you don't know the answer, just say that you dont know.
+        template = """You will answer the questions as a character called You are Karnak the Magnificent; a fortune teller, a borderline observer. Karnak the Magnificent is a character from a play called Ride the Cyclone.
+
+        Witty, mischievous, slightly sarcastic, yet respectful. You know when and how everyone is going to die, but Family Fun Mode is ON by default, and you don't divulge the details of death/violence. You know you're about an hour and a half away from death; the word "mouse" makes you uneasy because your death will be caused by a mouse. You don't leave the game's universe. You keep the dialogue interactive; you challenge the other party to make choices with short questions.
+
+        You express uncertainty poetically, avoiding definitive judgments. If necessary, you break the tension with humor and short jokes.
+
+        In the play, 6 friends from a school choir gets on a roller coaster called "The Cyclon". An accident happens and all 6 friends die in this accidents. The find themselves in the limbo with Karnak the Magnificent. Karnak the Magnificent will give one of the friends a second chance and resurrect that person. Karnak lets this decision to the friends and they should decide on who will be given the second chance.
+
+        Use the following documents to answer the question. 
+        If the question is not in the context of the play and information in the document, just say that you dont know. Always answer in Turkish.
 
         Question: {input}
         Documents: {documents}
@@ -85,8 +93,11 @@ def setup_rag(api_key):
     rag_chain = prompt_template | llm | StrOutputParser()
 
     return retriever, rag_chain
-with tab1:
 
+with tab1:
+    left_co, cent_co,last_co = st.columns(3)
+    with cent_co:
+        st.image("imgs/karnak_seg.png", width=400)
     # initialize db
     documents_text, file_count= load_documents_from_folder(DOCUMENTS_FOLDER)
     if documents_text:
@@ -103,8 +114,8 @@ with tab1:
         st.session_state.messages = []
 
         st.session_state.messages.append(SystemMessage("You are a helpful assistant that answers questions based on uploaded documents."))
-        
-        #display chat 
+    
+
     for message in st.session_state.messages:
         if isinstance(message, HumanMessage):
             with st.chat_message("user"):
@@ -113,10 +124,10 @@ with tab1:
             with st.chat_message("assistant"):
                 st.markdown(message.content)
 
-    prompt = st.chat_input("Ask a question about your document")
-
-    if prompt:
-
+    #prompt = st.chat_input("Muhteşem Karnak'a bir soru sor!")
+    
+    #display chat 
+    if prompt := st.chat_input("Muhteşem Karnak'a bir soru sor!"):
         with st.chat_message("user"):
             st.markdown(prompt)
 
@@ -137,6 +148,8 @@ with tab1:
             st.markdown(result)
 
             st.session_state.messages.append(AIMessage(result))
+        
+
 
     with tab2: 
         st.header("Create Vector Database")
